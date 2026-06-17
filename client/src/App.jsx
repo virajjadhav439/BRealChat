3b82528ce6c26a4cf85ac32c1a802eadb10a9170
virajjadhav439 <virajjadhav439@gmail.com>
2026-06-17 18:17:50 +0530

    Ready for Deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Login from './pages/Login'
